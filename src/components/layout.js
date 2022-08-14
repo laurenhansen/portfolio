e4d13cb851489cca071e78eb2d895a5c69af2444
Lauren Hansen <lauren.hansen@gmail.com>
2022-08-14 13:32:08 -0500

    adding navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import SiteNav from './nav.js'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import "./layout.scss"
 
@@ -17,27 +18,16 @@ const Layout = ({ pageTitle, children }) => {
     <div class="page-container">
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
       
-      <div class="nav-wrapper">
+      
         <header>
             <h1>{data.site.siteMetadata.title}</h1>
             <h3>UX Designer | Chicago, IL</h3>
         </header>
-        <nav>
-        <ul class="nav-links">
-          <li class="nav-link-item">
-            <Link to="/" class="nav-link-text" activeClassName="active">
-              Home
-            </Link>
-          </li>
-          
-          <li class="nav-link-item">
-            <Link to="/portfolio" class="nav-link-text" activeClassName="active">
-              Portfolio
-            </Link>
-          </li>
-        </ul>
-        </nav>
-      </div>
+      <SiteNav></SiteNav>
+      
+    
+         
+      
       <div class="content-wrapper">
         <main>
           {children}
---
+++ src/components/nav.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Link } from 'gatsby'
+
+const SiteNav = () => {
+    const [isActive, setActive] = useState("false");
+  
+    const handleToggle = () => {
+      setActive(!isActive);
+    };
+
+
+    return (
+        <nav id="main-nav" className={`site-nav ${isActive ? "" : "open"}`}>
+        <a class="menu-toggle" onClick={handleToggle}>
+                  <div class="bar"></div>
+                  <div class="bar"></div>
+                  <div class="bar"></div>
+        </a>
+              <ul>
+                  <li><Link to="/" class="nav-link-text" activeClassName="active">
+                Home
+              </Link></li>
+                  <li><Link to="/portfolio" class="nav-link-text" activeClassName="active">
+                Portfolio
+              </Link></li>
+              </ul>
+          </nav>
+      );
+}
+
+export default SiteNav
